c4bd00bf5648afb70ed073c2e0de924f96e93c58
Jo-Jaehyeon <jerryjo0906@gmail.com>
2026-03-20 05:07:20 +0900

    [Init] 초기 설정

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Types.h"
+
+#include <iostream>
+
+// SQL
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+// boost
+#include <boost/asio.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/algorithm//string/split.hpp>
+#include <boost/algorithm//string/classification.hpp>
+
+// spdlog
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/daily_file_sink.h"
+
+void HelloWorld();
