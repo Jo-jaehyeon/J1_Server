9817e111789a0b410aaf190c49a72485a12a988c
Jo-Jaehyeon <jerryjo0906@gmail.com>
2026-08-11 05:04:52 +0900

    [fix] 경매장 서버 분리를 위한 패킷 삭제

--- GameServer/Network/Handlers/AuctionPacketHandler.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "pch.h"
-#include "AuctionPacketHandler.h"
-
-bool Handle_REQ_OPENAUCTION(SessionPtr& session, Game::REQ_OPENAUCTION& pkt)
-{
-	// TODO: Auction 관련 로직 작성
-	return true;
-}
-
--- GameServer/Network/Handlers/AuctionPacketHandler.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "../Protocol/AuctionProtocol.pb.h"
-#include "Packet.h"
-
-#if UE_BUILD_DEBUG + UE_BUILD_DEVELOPMENT + UE_BUILD_TEST + UE_BUILD_SHIPPING >= 1
-#include "J1.h"
-#endif
-
-bool Handle_REQ_OPENAUCTION(SessionPtr& session, Game::REQ_OPENAUCTION& pkt);
